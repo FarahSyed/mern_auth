8a588f9e020eecbbd24dc4b13269a9a312da0e3e
FarahSyed <farahsyed44@gmail.com>
2024-03-06 22:50:05 +0500

    connect to db

--- api/app.js
+++ api/app.js
@@ -1,8 +1,19 @@
 import express from "express";
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 
 app.listen(PORT, () => {
