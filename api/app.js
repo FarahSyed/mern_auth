c88be3d0a4807281893b8e766b37f5eb7315ef96
FarahSyed <farahsyed44@gmail.com>
2024-03-09 04:59:24 +0500

    add a middleware and a function to handle errors

--- api/app.js
+++ api/app.js
@@ -22,6 +22,16 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server listening at PORT ${PORT}`);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,22 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-    const { userName, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ user_name: userName, email, password: hashedPassword });
+export const signup = async (req, res, next) => {
+    const { userName, email, password } = req.body || {};
+    if(!userName || !email || !password) {
+        next(errorHandler(403, "Required fields are missing"));
+        return;
+    }
+
     try {
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = new User({ user_name: userName, email, password: hashedPassword });
         await newUser.save();
         res.status(201).json({
             message: "User Signed Up Successfully",
         });
     } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        });
+        next(error);
     }
 }
