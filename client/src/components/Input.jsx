d4fc3e65cf113e3cb2bf4f4336d59cbe11ad5e1d
FarahSyed <farahsyed44@gmail.com>
2024-03-18 02:11:06 +0500

    fix small bug

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Input(props) {
     const { type, placeholder, name, id, required, className, onChange, onFocus, defaultValue, ref, hidden, accept } = props;
     return (
-        <input type={type} placeholder={placeholder} name={name} id={id} required={required} onChange={onChange} onFocus={onFocus} defaultValue={defaultValue} ref={ref} hidden={hidden} accept={accept} className={`${className} bg-slate-100 p-3 rounded-lg`} />
+        <input type={type} placeholder={placeholder} name={name} id={id} required={required} onChange={onChange} ref={ref} hidden={hidden} accept={accept} onFocus={onFocus} defaultValue={defaultValue} className={`${className} bg-slate-100 p-3 rounded-lg`} />
     )
 }
 
