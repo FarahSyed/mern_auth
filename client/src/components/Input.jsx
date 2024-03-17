b219db989bb89b097d7c2c58eb9dc4d225e0e83b
FarahSyed <farahsyed44@gmail.com>
2024-03-17 23:51:00 +0500

    complete upload image functionality

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Input(props) {
-    const { type, placeholder, name, id, required, onChange, onFocus, defaultValue } = props;
+    const { type, placeholder, name, id, required, className, onChange, onFocus, defaultValue, ref, hidden, accept } = props;
     return (
-        <input type={type} placeholder={placeholder} name={name} id={id} required={required} onChange={onChange} onFocus={onFocus} defaultValue={defaultValue} className='bg-slate-100 p-3 rounded-lg' />
+        <input type={type} placeholder={placeholder} name={name} id={id} required={required} onChange={onChange} onFocus={onFocus} defaultValue={defaultValue} ref={ref} hidden={hidden} action={action} className={`${className} bg-slate-100 p-3 rounded-lg`} />
     )
 }
 
