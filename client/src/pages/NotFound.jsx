53a9f5ef61299f382041c987057d46ebec4ecc0a
FarahSyed <farahsyed44@gmail.com>
2024-03-22 22:47:04 +0500

    complete home, about and not found pages

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 function NotFound() {
   return (
-    <>
-        <h1>Page Not Found</h1>
-    </>
+    <div className='h-screen flex flex-col justify-center items-center'>
+        <h1 className='mb-4 text-5xl md:text-7xl font-bold text-slate-800'>404 Error</h1>
+        <h1 className='text-2xl md:text-4xl font-bold text-slate-800 uppercase'>Page Not Found</h1>
+    </div>
   )
 }
 
